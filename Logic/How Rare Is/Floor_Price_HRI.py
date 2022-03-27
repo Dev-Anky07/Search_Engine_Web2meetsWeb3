02d9d821998c12b328fb8541d3389c4958f48ef9
Dev-Anky07 <90901237+Dev-Anky07@users.noreply.github.com>
2022-03-27 20:35:45 +0530

    Ceated a get floor function for How Rare Is API

---
+++ Logic/How Rare Is/Floor_Price_HRI.py
@@ -0,0 +1,30 @@
+# Result of successful data query:
+# ## https://api.howrare.is/v0.1/floor call api use Get_Floor() function
+
+# Create a function for it next to call the api and return data
+
+# Result of successful data query:
+{
+    "api_version": "0.1",
+    "result": {
+        "api_code": 200,
+        "api_response": "Success",
+        "data": [
+            {
+                "name": "Degen Ape Academy",
+                "url": "/degenapes",
+                "floor_sol": "24.99",
+                "floor_usd": "5596.01"
+            },
+            {
+                "name": "SolanaMonkeyBusiness (SMB)",
+                "url": "/smb",
+                "floor_sol": "138.00",
+                "floor_usd": "30902.34"
+            }
+        ]
+    }
+}
+                    
+# Data in the result trimmed to 2 items and actual result would Dish out all the data there is and 
+# we'll have to parse it.
